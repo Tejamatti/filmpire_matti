1d3a2dbd97fa35ce46ea7d303aa824c0ae838b9d
Tejamatti <tejmatti53@gmail.com>
2024-06-27 17:10:27 +0530

    First commit

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,13 +1,43 @@
+// const reportWebVitals = onPerfEntry => {
+//   if (onPerfEntry && onPerfEntry instanceof Function) {
+//     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+//       getCLS(onPerfEntry);
+//       getFID(onPerfEntry);
+//       getFCP(onPerfEntry);
+//       getLCP(onPerfEntry);
+//       getTTFB(onPerfEntry);
+//     });
+//   }
+// };
+
+// export default reportWebVitals;
+
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
+      getCLS(metric => {
+        console.log('CLS:', metric.value);
+        onPerfEntry(metric);
+      });
+      getFID(metric => {
+        console.log('FID:', metric.value);
+        onPerfEntry(metric);
+      });
+      getFCP(metric => {
+        console.log('FCP:', metric.value);
+        onPerfEntry(metric);
+      });
+      getLCP(metric => {
+        console.log('LCP:', metric.value);
+        onPerfEntry(metric);
+      });
+      getTTFB(metric => {
+        console.log('TTFB:', metric.value);
+        onPerfEntry(metric);
+      });
     });
   }
 };
 
 export default reportWebVitals;
+
